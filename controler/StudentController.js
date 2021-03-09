564aca9e389266cfa62a44b3ffdd9c06776bc622
nipulviduranga <71004802+nipulviduranga@users.noreply.github.com>
2021-03-09 08:43:59 +0530

    final Without Login

--- controler/StudentController.js
+++ controler/StudentController.js
@@ -11,15 +11,104 @@ if (reqOfother==='Grade06Term1'){
     return StudentSchema.MarksGrade06Term3;
 }else if (reqOfother==='none'){
     return StudentSchema.None;
+}else if (reqOfother==='Grade07Term1'){
+    return StudentSchema.MarksGrade07Term1;
+}else if (reqOfother==='Grade07Term2'){
+    return StudentSchema.MarksGrade07Term2;
+}else if (reqOfother==='Grade07Term3'){
+    return StudentSchema.MarksGrade07Term3;
+}else if (reqOfother==='Grade08Term1'){
+    return StudentSchema.MarksGrade08Term1;
+}else if (reqOfother==='Grade08Term2'){
+    return StudentSchema.MarksGrade08Term2;
+}else if (reqOfother==='Grade08Term3'){
+    return StudentSchema.MarksGrade08Term3;
+}else if (reqOfother==='Grade09Term1'){
+    return StudentSchema.MarksGrade09Term1;
+}else if (reqOfother==='Grade09Term2'){
+    return StudentSchema.MarksGrade09Term2;
+}else if (reqOfother==='Grade09Term3'){
+    return StudentSchema.MarksGrade09Term3;
+}else if (reqOfother==='Grade10Term1'){
+    return StudentSchema.MarksGrade10Term1;
+}else if (reqOfother==='Grade10Term2'){
+    return StudentSchema.MarksGrade10Term2;
+}else if (reqOfother==='Grade10Term3'){
+    return StudentSchema.MarksGrade10Term3;
+}else if (reqOfother==='Grade11Term1'){
+    return StudentSchema.MarksGrade11Term1;
+}else if (reqOfother==='Grade11Term2'){
+    return StudentSchema.MarksGrade11Term2;
+}else if (reqOfother==='Grade11Term3'){
+    return StudentSchema.MarksGrade11Term3;
+}else if (reqOfother==='Grade12ArtTerm1'){
+    return StudentSchema.MarksGrade12ArtTerm1;
+}else if (reqOfother==='Grade12ArtTerm2'){
+    return StudentSchema.MarksGrade12ArtTerm2;
+}else if (reqOfother==='Grade12ArtTerm3'){
+    return StudentSchema.MarksGrade12ArtTerm3;
+}else if (reqOfother==='Grade12ComTerm1'){
+    return StudentSchema.MarksGrade12ComTerm1;
+}else if (reqOfother==='Grade12ComTerm2'){
+    return StudentSchema.MarksGrade12ComTerm2;
+}else if (reqOfother==='Grade12ComTerm3'){
+    return StudentSchema.MarksGrade12ComTerm3;
+}else if (reqOfother==='Grade12TecTerm1'){
+    return StudentSchema.MarksGrade12TecTerm1;
+}else if (reqOfother==='Grade12TecTerm2'){
+    return StudentSchema.MarksGrade12TecTerm2;
+}else if (reqOfother==='Grade12TecTerm3'){
+    return StudentSchema.MarksGrade12TecTerm3;
+}else if (reqOfother==='Grade12MathsTerm1'){
+    return StudentSchema.MarksGrade12MathsTerm1;
+}else if (reqOfother==='Grade12MathsTerm2'){
+    return StudentSchema.MarksGrade12MathsTerm2;
+}else if (reqOfother==='Grade12MathsTerm3'){
+    return StudentSchema.MarksGrade12MathsTerm3;
+}else if (reqOfother==='Grade12BioTerm1'){
+    return StudentSchema.MarksGrade12BioTerm1;
+}else if (reqOfother==='Grade12BioTerm2'){
+    return StudentSchema.MarksGrade12BioTerm2;
+}else if (reqOfother==='Grade12BioTerm3'){
+    return StudentSchema.MarksGrade12BioTerm3;
+}else if (reqOfother==='Grade13ArtTerm1'){
+    return StudentSchema.MarksGrade13ArtTerm1;
+}else if (reqOfother==='Grade13ArtTerm2'){
+    return StudentSchema.MarksGrade13ArtTerm2;
+}else if (reqOfother==='Grade13ArtTerm3'){
+    return StudentSchema.MarksGrade13ArtTerm3;
+}else if (reqOfother==='Grade13ComTerm1'){
+    return StudentSchema.MarksGrade13ComTerm1;
+}else if (reqOfother==='Grade13ComTerm2'){
+    return StudentSchema.MarksGrade13ComTerm2;
+}else if (reqOfother==='Grade13ComTerm3'){
+    return StudentSchema.MarksGrade13ComTerm3;
+}else if (reqOfother==='Grade13TecTerm1'){
+    return StudentSchema.MarksGrade13TecTerm1;
+}else if (reqOfother==='Grade13TecTerm2'){
+    return StudentSchema.MarksGrade13TecTerm2;
+}else if (reqOfother==='Grade13TecTerm3'){
+    return StudentSchema.MarksGrade13TecTerm3;
+}else if (reqOfother==='Grade13MathsTerm1'){
+    return StudentSchema.MarksGrade13MathsTerm1;
+}else if (reqOfother==='Grade13MathsTerm2'){
+    return StudentSchema.MarksGrade13MathsTerm2;
+}else if (reqOfother==='Grade13MathsTerm3'){
+    return StudentSchema.MarksGrade13MathsTerm3;
+}else if (reqOfother==='Grade13BioTerm1'){
+    return StudentSchema.MarksGrade13BioTerm1;
+}else if (reqOfother==='Grade13BioTerm2'){
+    return StudentSchema.MarksGrade13BioTerm2;
+}else if (reqOfother==='Grade13BioTerm3'){
+    return StudentSchema.MarksGrade13BioTerm3;
 }
-
 }
 const saveStudent = (req,res) =>{
-    console.log(req.body.studentName,req.body.subHistory,req.body.subMaths,req.body.subSinhala,req.body.subHealth,req.body.subCitizen,req.body.subScience,req.body.subBuddhism,req.body.subGeography,req.body.subIt,req.body.subPTS,req.body.subAesrhetics,req.body.totals);
 
-    const Marks6to9 = new StudentSchema.MarksGrade06Term3(
+
+    const MarksOL = new schemaController()(
         {
-            StudentName:req.body.studentName,
+          StudentName:req.body.studentName,
             SubMaths :req.body.subMaths,
             SubSinhala:req.body.subSinhala,
             SubEnglish:req.body.subEnglish,
@@ -33,11 +122,39 @@ const saveStudent = (req,res) =>{
             SubHistory:req.body.subHistory,
             SubTamil:req.body.subTamil,
             SubAesrhetics:req.body.subAesrhetics,
-            Total : req.body.totals
+            SubGroup1 : req.body.Group1,
+            SubGroup2 : req.body.Group2,
+            SubGroup3 : req.body.Group3,
+
+            Total : req.body.totals,
+
+            SubCom_Maths:req.body.subCom_Maths,
+            SubPhysics:req.body.subPhysics,
+          SubChemistry:req.body.subChemistry,
+            SubICT:req.body.subICT,
+            SubBio:req.body.subBio,
+            SubET:req.body.subET,
+            SubSFT:req.body.subSFT,
+            SubAgry:req.body.subAgry,
+            SubBST:req.body.subBST,
+            SubAccounting:req.body.subAccounting,
+            SubBS:req.body.subBS,
+            SubEconomics:req.body.subEconomics,
+            /*SubSinhala:req.body.subSinhala,*/
+            SubLogic:req.body.subLogic,
+            /*SubGeography :req.body.subGeography,*/
+            SubPolitics:req.body.subPolitics,
+            SubBC:req.body.subBC,
+            SubLanguages:req.body.subLanguages,
+            /*SubHistory:req.body.subHistory,*/
+            SubAesthetic:req.body.subAesthetic,
+            /*SubEnglish:req.body.subEnglish,*/
+            SubGIT : req.body.subGIT,
+            SubBusinessStac:req.body.subBusinessStac
         }
 
 );
-    Marks6to9.save().then(result => {
+    MarksOL.save().then(result => {
         res.status(200).json({isSaved : true,data : result});
     }).catch(error => {
         res.status(500).json(error);
@@ -81,7 +198,7 @@ const deleteStudent = (req,res)=>{
 const updateStudent = (req,res)=> {
     console.log(req.body.id);
     schemaController().updateOne({ _id:req.body.id},{$set:{
-            StudentName:req.body.studentName,
+          StudentName:req.body.studentName,
             SubMaths :req.body.subMaths,
             SubSinhala:req.body.subSinhala,
             SubEnglish:req.body.subEnglish,
@@ -95,7 +212,34 @@ const updateStudent = (req,res)=> {
             SubHistory:req.body.subHistory,
             SubTamil:req.body.subTamil,
             SubAesrhetics:req.body.subAesrhetics,
+            SubGroup1 : req.body.Group1,
+            SubGroup2 : req.body.Group2,
+            SubGroup3 : req.body.Group3,
             Total : req.body.totals,
+
+            SubCom_Maths:req.body.subCom_Maths,
+            SubPhysics:req.body.subPhysics,
+            SubChemistry:req.body.subChemistry,
+            SubICT:req.body.subICT,
+            SubBio:req.body.subBio,
+            SubET:req.body.subET,
+            SubSFT:req.body.subSFT,
+            SubAgry:req.body.subAgry,
+            SubBST:req.body.subBST,
+            SubAccounting:req.body.subAccounting,
+            SubBS:req.body.subBS,
+            SubEconomics:req.body.subEconomics,
+            /*SubSinhala:req.body.subSinhala,*/
+            SubLogic:req.body.subLogic,
+            /*SubGeography :req.body.subGeography,*/
+            SubPolitics:req.body.subPolitics,
+            SubBC:req.body.subBC,
+            SubLanguages:req.body.subLanguages,
+            /*SubHistory:req.body.subHistory,*/
+            SubAesthetic:req.body.subAesthetic,
+            /*SubEnglish:req.body.subEnglish,*/
+            SubGIT : req.body.subGIT,
+            SubBusinessStac:req.body.subBusinessStac,
             Other : []
     }}).then(result=>{
 
--- model/StudentDTO.js
+++ model/StudentDTO.js
@@ -20,7 +20,7 @@ const StudentSchema = new mongoose.Schema({
     },
     SubHealth: {
         type: Number,
-        required: true
+
     },
     SubScience: {
         type: Number,
@@ -32,19 +32,19 @@ const StudentSchema = new mongoose.Schema({
     },
     SubCitizen: {
         type: Number,
-        required: true
+
     },
     SubGeography: {
         type: Number,
-        required: true
+
     },
     SubIt: {
         type: Number,
-        required: true
+
     },
     SubPTS: {
         type: Number,
-        required: true
+
     },
     SubHistory: {
         type: Number,
@@ -52,11 +52,20 @@ const StudentSchema = new mongoose.Schema({
     },
     SubTamil: {
         type: Number,
-        required: true
+
     },
-    SubAesrhetics: {
+    SubAesrhetic: {
         type: Number,
-        required: true
+
+    },
+    SubGroup1:{
+     type:Number
+    },
+    SubGroup2:{
+        type:Number
+    },
+    SubGroup3:{
+        type:Number
     },
     Total: {
         type: Number,
@@ -66,14 +75,151 @@ const StudentSchema = new mongoose.Schema({
         type:Array
     }
 });
+const StudentSchemaAL= new mongoose.Schema({
+    StudentName: {
+        type: String,
+        required: true
+    },
+    SubCom_Maths: {
+        type: Number
+    },
+    SubPhysics: {
+        type: Number
+    },
+    SubChemistry: {
+        type: Number
+    },
+    SubICT: {
+        type: Number
+    },
+    SubBio: {
+        type: Number
+    },
+    SubET: {
+        type: Number
+    },
+    SubSFT: {
+        type: Number
+    },
+    SubAgry: {
+        type: Number
+    },
+    SubBST: {
+        type: Number
+    },
+    SubAccounting: {
+        type: Number
+    },
+    SubBS: {
+        type: Number
+    },
+    SubEconomics: {
+        type: Number
+    },
+    SubSinhala: {
+        type: Number
+    },
+    SubLogic: {
+        type: Number
+    },
+    SubGeography: {
+        type: Number
+    },
+    SubPolitics: {
+        type: Number
+    },
+    SubBC: {
+        type: Number
+    },
+    SubLanguages: {
+        type: Number
+    },
+    SubHistory: {
+        type: Number
+    },
+    SubAesthetic: {
+        type: Number
+    },
+    SubEnglish: {
+        type: Number
+    },
+    SubGIT: {
+        type: Number
+    },
+    SubBusinessStac:{
+        type: Number
+    },
+    Total: {
+        type: Number,
+
+    },
+    other:{
+        type:Array
+    }
+
+});
+
+
 
 const None =mongoose.model('None', StudentSchema);
 const MarksGrade06Term1=mongoose.model('MarksGrade06Term1', StudentSchema);
 const MarksGrade06Term2=mongoose.model('MarksGrade06Term2', StudentSchema);
 const MarksGrade06Term3=mongoose.model('MarksGrade06Term3', StudentSchema);
+const MarksGrade07Term1=mongoose.model('MarksGrade07Term1', StudentSchema);
+const MarksGrade07Term2=mongoose.model('MarksGrade07Term2', StudentSchema);
+const MarksGrade07Term3=mongoose.model('MarksGrade07Term3', StudentSchema);
+const MarksGrade08Term1=mongoose.model('MarksGrade08Term1', StudentSchema);
+const MarksGrade08Term2=mongoose.model('MarksGrade08Term2', StudentSchema);
+const MarksGrade08Term3=mongoose.model('MarksGrade08Term3', StudentSchema);
+const MarksGrade09Term1=mongoose.model('MarksGrade09Term1', StudentSchema);
+const MarksGrade09Term2=mongoose.model('MarksGrade09Term2', StudentSchema);
+const MarksGrade09Term3=mongoose.model('MarksGrade09Term3', StudentSchema);
+const MarksGrade10Term1=mongoose.model('MarksGrade10Term1', StudentSchema);
+const MarksGrade10Term2=mongoose.model('MarksGrade10Term2', StudentSchema);
+const MarksGrade10Term3=mongoose.model('MarksGrade10Term3', StudentSchema);
+const MarksGrade11Term1=mongoose.model('MarksGrade11Term1', StudentSchema);
+const MarksGrade11Term2=mongoose.model('MarksGrade11Term2', StudentSchema);
+const MarksGrade11Term3=mongoose.model('MarksGrade11Term3', StudentSchema);
+const MarksGrade12ArtTerm1=mongoose.model('MarksGrade12ArtTerm1', StudentSchemaAL);
+const MarksGrade12ArtTerm2=mongoose.model('MarksGrade12ArtTerm2', StudentSchemaAL);
+const MarksGrade12ArtTerm3=mongoose.model('MarksGrade12ArtTerm3', StudentSchemaAL);
+const MarksGrade12ComTerm1=mongoose.model('MarksGrade12ComTerm1', StudentSchemaAL);
+const MarksGrade12ComTerm2=mongoose.model('MarksGrade12ComTerm2', StudentSchemaAL);
+const MarksGrade12ComTerm3=mongoose.model('MarksGrade12ComTerm3', StudentSchemaAL);
+const MarksGrade12TecTerm1=mongoose.model('MarksGrade12TecTerm1', StudentSchemaAL);
+const MarksGrade12TecTerm2=mongoose.model('MarksGrade12TecTerm2', StudentSchemaAL);
+const MarksGrade12TecTerm3=mongoose.model('MarksGrade12TecTerm3', StudentSchemaAL);
+const MarksGrade12MathsTerm1=mongoose.model('MarksGrade12MathsTerm1', StudentSchemaAL);
+const MarksGrade12MathsTerm2=mongoose.model('MarksGrade12MathsTerm2', StudentSchemaAL);
+const MarksGrade12MathsTerm3=mongoose.model('MarksGrade12MathsTerm3', StudentSchemaAL);
+const MarksGrade12BioTerm1=mongoose.model('MarksGrade12BioTerm1', StudentSchemaAL);
+const MarksGrade12BioTerm2=mongoose.model('MarksGrade12BioTerm2', StudentSchemaAL);
+const MarksGrade12BioTerm3=mongoose.model('MarksGrade12BioTerm3', StudentSchemaAL);
+const MarksGrade13ArtTerm1=mongoose.model('MarksGrade13ArtTerm1', StudentSchemaAL);
+const MarksGrade13ArtTerm2=mongoose.model('MarksGrade13ArtTerm2', StudentSchemaAL);
+const MarksGrade13ArtTerm3=mongoose.model('MarksGrade13ArtTerm3', StudentSchemaAL);
+const MarksGrade13ComTerm1=mongoose.model('MarksGrade13ComTerm1', StudentSchemaAL);
+const MarksGrade13ComTerm2=mongoose.model('MarksGrade13ComTerm2', StudentSchemaAL);
+const MarksGrade13ComTerm3=mongoose.model('MarksGrade13ComTerm3', StudentSchemaAL);
+const MarksGrade13TecTerm1=mongoose.model('MarksGrade13TecTerm1', StudentSchemaAL);
+const MarksGrade13TecTerm2=mongoose.model('MarksGrade13TecTerm2', StudentSchemaAL);
+const MarksGrade13TecTerm3=mongoose.model('MarksGrade13TecTerm3', StudentSchemaAL);
+const MarksGrade13MathsTerm1=mongoose.model('MarksGrade13MathsTerm1', StudentSchemaAL);
+const MarksGrade13MathsTerm2=mongoose.model('MarksGrade13MathsTerm2', StudentSchemaAL);
+const MarksGrade13MathsTerm3=mongoose.model('MarksGrade13MathsTerm3', StudentSchemaAL);
+const MarksGrade13BioTerm1=mongoose.model('MarksGrade13BioTerm1', StudentSchemaAL);
+const MarksGrade13BioTerm2=mongoose.model('MarksGrade13BioTerm2', StudentSchemaAL);
+const MarksGrade13BioTerm3=mongoose.model('MarksGrade13BioTerm3', StudentSchemaAL);
+
 
 module.exports ={
-    MarksGrade06Term1,MarksGrade06Term2,MarksGrade06Term3,None
+    MarksGrade06Term1,MarksGrade06Term2,MarksGrade06Term3,None,MarksGrade07Term1,MarksGrade07Term2,MarksGrade07Term3,MarksGrade08Term1,MarksGrade08Term2,MarksGrade08Term3,MarksGrade09Term1,MarksGrade09Term2,MarksGrade09Term3,MarksGrade10Term1,MarksGrade10Term2,MarksGrade10Term3,
+    MarksGrade11Term1,MarksGrade11Term2,MarksGrade11Term3,MarksGrade12ArtTerm1,MarksGrade12ArtTerm2,MarksGrade12ArtTerm3,
+    MarksGrade12BioTerm1,MarksGrade12BioTerm2,MarksGrade12BioTerm3,MarksGrade12ComTerm1,MarksGrade12ComTerm2,MarksGrade12ComTerm3,
+    MarksGrade12MathsTerm1,MarksGrade12MathsTerm2,MarksGrade12MathsTerm3,MarksGrade12TecTerm1,MarksGrade12TecTerm2,MarksGrade12TecTerm3,
+    MarksGrade13ArtTerm1,MarksGrade13ArtTerm2,MarksGrade13ArtTerm3,
+    MarksGrade13BioTerm1,MarksGrade13BioTerm2,MarksGrade13BioTerm3,MarksGrade13ComTerm1,MarksGrade13ComTerm2,MarksGrade13ComTerm3,
+    MarksGrade13MathsTerm1,MarksGrade13MathsTerm2,MarksGrade13MathsTerm3,MarksGrade13TecTerm1,MarksGrade13TecTerm2,MarksGrade13TecTerm3
 }
 
 
