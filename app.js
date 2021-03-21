d6828cb97e3be5c29e0f7e2c1ba6bdc4971892f5
nipulviduranga <71004802+nipulviduranga@users.noreply.github.com>
2021-03-21 11:05:22 +0530

    final with heroku4

--- app.js
+++ app.js
@@ -14,7 +14,24 @@ var app = express();
 
 // view engine setup
 
+app.use(function (req, res, next) {
 
+  // Website you wish to allow to connect
+  res.setHeader('Access-Control-Allow-Origin', 'https://nipulviduranga.github.io');
+
+  // Request methods you wish to allow
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+  // Request headers you wish to allow
+  res.setHeader('Access-Control-Allow-Headers', 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader('Access-Control-Allow-Credentials', true);
+
+  // Pass to next layer of middleware
+  next();
+});
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 app.use(bodyparser.json());
